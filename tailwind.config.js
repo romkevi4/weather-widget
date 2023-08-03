bc651de8c6aced1a79cd980ae39e3febe9f7bd31
Roman Shatskikh <shatskikh.roman@gmail.com>
2023-08-03 12:56:50 +0300

    feat: add tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './public/index.html',
+    './src/**/*.{vue,js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        custom: ['Inter', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
